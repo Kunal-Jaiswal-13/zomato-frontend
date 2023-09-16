a390456a10e3f7f40820293f11f362d8ddfd93c6
Kunal-Jaiswal-13 <jaiswalkunal1322003@gmail.com>
2023-09-16 18:42:55 +0530

    change in details page

--- src/component/details.js
+++ src/component/details.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "../styles/details.css";
-import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+// import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import Header from "./header";
 import queryString from "query-string";
@@ -25,10 +25,23 @@ const menuStyles = {
   },
 };
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "black",
+  },
+};
 class Details extends React.Component {
   constructor() {
     super();
     this.state = {
+      content_value: 1,
+      galleryModalIsOpen: false,
       restaurantData: {},
       menuModelIsOpen: false,
       menuItems: [],
@@ -65,10 +78,10 @@ class Details extends React.Component {
             updatedMenuItems.push(menuItem);
           });
         });
-        this.setState({ menuItems: updatedMenuItems })
+        this.setState({ menuItems: updatedMenuItems });
       })
       .catch((err) => console.log(err));
-    };
+  }
 
   handleOrder = (value) => {
     this.setState({
@@ -93,49 +106,157 @@ class Details extends React.Component {
     this.setState({ menuItems: items, subTotal: total });
   };
 
+  openOverview = () => {
+    this.setState({ content_value: 1 });
+  };
+
+  openContact = () => {
+    this.setState({ content_value: 2 });
+  };
+
+  handleGalleryModal = () => {
+    this.setState({ galleryModalIsOpen: true });
+  };
+
+  closeModal = () => {
+    this.setState({ galleryModalIsOpen: false });
+  };
   render() {
-    const { restaurantData, menuModelIsOpen, menuItems, subTotal } = this.state;
+    const {
+      restaurantData,
+      galleryModalIsOpen,
+      menuModelIsOpen,
+      menuItems,
+      subTotal,
+    } = this.state;
     return (
-      <>
+      <div>
         <Header />
-        <div className="details">
-          <h1>details page</h1>
-          <Carousel className="carousel" autoPlay showThumbs={false}>
-            <div>
-              <img className="image" alt="" src={`./${restaurantData.image}`} />
-            </div>
-          </Carousel>
-          <h1>{restaurantData.name}</h1>
+        <div className="container" style={{}}>
+          <img
+            src={restaurantData.image}
+            className="detail-img"
+            alt="restaurant-imag"
+            width="100%"
+            height="352px"
+            style={{ marginTop: "65px" }}
+          />
+          <button className="gallery-button" onClick={this.handleGalleryModal}>
+            Click to see Image Gallery
+          </button>
+          <div className="restaurant-name">{restaurantData.name}</div>
           <button
-            className="buttonForOrder "
+            className="order-button"
             onClick={() => this.handleOrder(true)}
           >
             Place Online Order
           </button>
-          <Tabs>
-            <TabList>
-              <Tab>Overview</Tab>
-              <Tab>Contact</Tab>
-            </TabList>
-            <TabPanel>
-              <h2>About this place</h2>
-              <br />
-              <h3>cuisine</h3>
-              <p>Bakery Fast Food</p>
-              <h3>Average Cost</h3>
-              <p>Rs. {restaurantData.min_price} for two people(approx.)</p>
-            </TabPanel>
-            <TabPanel>
-              <h2>contact Number</h2>
-              <p>{restaurantData.contact_number}</p>
-              <h3>{restaurantData.name}</h3>
-              <h3>
-                {restaurantData.address
-                  ? restaurantData?.address
-                  : "shop 1,plot 13,samruddhi complex,Chincholi"}
-              </h3>
-            </TabPanel>
-          </Tabs>
+          <div className="btn-box">
+            <button
+              className="btn-1"
+              style={
+                this.state.content_value === 1
+                  ? { color: "#ce0505" }
+                  : { color: "black" }
+              }
+              onClick={this.openOverview}
+            >
+              Overview
+            </button>
+            <button
+              className="btn-2"
+              style={
+                this.state.content_value === 2
+                  ? { color: "#ce0505" }
+                  : { color: "black" }
+              }
+              onClick={this.openContact}
+            >
+              Contact
+            </button>
+          </div>
+          <div className="path"></div>
+          <div
+            id="content1"
+            className="content"
+            style={
+              this.state.content_value === 1
+                ? { display: "block" }
+                : { display: "none" }
+            }
+          >
+            <div
+              className="restaurant-name2"
+              style={{ marginTop: "20px", marginBottom: "30px" }}
+            >
+              About this place
+            </div>
+            <div className="cuisine-detail">Cuisine</div>
+            <div className="address" style={{ marginBottom: "30px" }}>
+              {restaurantData.cuisine_id
+                ? restaurantData.cuisine_id.map((item) => `${item.name} `)
+                : null}
+            </div>
+            <div className="cuisine-detail">Average Cost</div>
+            <div className="address" style={{ marginBottom: "0px" }}>
+              ₹{restaurantData.min_price} for two people (approx)
+            </div>
+          </div>
+          <div
+            id="content2"
+            className="content"
+            style={
+              this.state.content_value === 2
+                ? { display: "block" }
+                : { display: "none" }
+            }
+          >
+            <div className="phone-number" style={{ marginTop: "30px" }}>
+              Phone Number
+            </div>
+            <div className="number" style={{ marginBottom: "30px" }}>
+              {restaurantData.contact_number}
+            </div>
+            <div className="restaurant-name2">{restaurantData.name}</div>
+            <div className="address" style={{ marginBottom: "50px" }}>
+              {restaurantData.address}
+            </div>
+          </div>
+          <Modal isOpen={galleryModalIsOpen} style={customStyles}>
+            <button
+              className="carousel-closebutton"
+              style={{ float: "right", borderRadius: "50%" }}
+              onClick={this.closeModal}
+            >
+              <span
+                style={{ padding: "7px" }}
+                className="glyphicon glyphicon-remove"
+              ></span>
+            </button>
+            <div>
+              <Carousel showThumbs={false}>
+                <div>
+                  <img alt="" src="assests/breakfast.jpg" className="carousel-image" />
+                </div>
+                <div>
+                  <img alt=""  src="assests/lunch.jpg" className="carousel-image" />
+                </div>
+                <div>
+                  <img alt=""  src="assests/dinner.jpg" className="carousel-image" />
+                </div>
+                <div>
+                  <img alt="" src="assests/snacks.jpg" className="carousel-image" />
+                </div>
+                <div>
+                  <img alt="" src="assests/drink.jpg" className="carousel-image" />
+                </div>
+                <div>
+                  <img alt="" src="assests/nightlife.jpg" className="carousel-image" />
+                </div>
+              </Carousel>
+            </div>
+          </Modal>
+
           <Modal isOpen={menuModelIsOpen} style={menuStyles}>
             <div>
               <div
@@ -147,14 +268,17 @@ class Details extends React.Component {
               <br />
 
               <div>
-                <h3 className="restaurant-name">{restaurantData.name}</h3>
-                <h3 className="item-total">Subtotal: {subTotal}</h3>
-                <button className="btn btn-danger order-button">Pay Now</button>
+                <h3>{restaurantData.name}</h3>
+                <h3>Subtotal: {subTotal}</h3>
+                <button className="btn btn-danger">Pay Now</button>
                 {menuItems &&
                   menuItems.map((item, index) => {
                     return (
                       <div key={index}>
-                        <div className="row" style={{borderBottom:"1px solid black" }}>
+                        <div
+                          className="row"
+                          style={{ borderBottom: "1px solid black" }}
+                        >
                           <div className="col-8">
                             <h5>{item.name}</h5>
                             <h5>&#8377;{item.price}</h5>
@@ -240,7 +364,7 @@ class Details extends React.Component {
             </div>
           </Modal>
         </div>
-      </>
+      </div>
     );
   }
 }
